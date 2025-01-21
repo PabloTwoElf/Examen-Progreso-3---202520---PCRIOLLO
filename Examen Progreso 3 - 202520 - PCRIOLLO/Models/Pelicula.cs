788721ea764423a9095c98501dcc3aa7fee2d4cb
PabloTwoElf <pablo.escoba2005@gmail.com>
2025-01-21 07:49:26 -0500

    Creacion de Model y de Services

---
+++ Examen Progreso 3 - 202520 - PCRIOLLO/Models/Pelicula.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+namespace Examen_Progreso_3___202520___PCRIOLLO.Models
+{
+    public class Pelicula
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string TituloPelicula { get; set; }
+        public string Genero { get; set; }
+        public string ActorPrincipal { get; set; }
+        public string Awards { get; set; }
+        public string Website { get; set; }
+
+        public string NombreUsuario = "Pablo Criollo";
+
+    }
+}
---
+++ Examen Progreso 3 - 202520 - PCRIOLLO/ViewModel/buscarPeliculaCriolloViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Examen_Progreso_3___202520___PCRIOLLO.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_Progreso_3___202520___PCRIOLLO.ViewModel
+{
+    public class buscarPeliculaCriolloViewModel : BindableObject
+    {
+        private readonly PeliculaBuscarServices _peliculaBuscarServices;
+
+        private string _tituloPelicula;
+        public string _resultado;
+        public string TituloPelicula
+        {
+            get => _tituloPelicula;
+            set
+            {
+                _tituloPelicula = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Resultado
+        {
+            get => _resultado;
+            set
+            {
+                _resultado = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Observable
+
+
+    }
+}
